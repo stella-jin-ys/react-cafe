0175e308e5ad9bdef33eef193b64c44e9f98ae7a
Johan Cederblad <johanzcederblad@hotmail.com>
2025-01-21 09:18:32 +0100

    Review and Contact ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Contact from "./pages/Contact";
-
+import Review from "./components/Review";
 
 const App: React.FC = () => {
   return <div>
           <Routes>
             <Route path="/" element={<Contact />}></Route>
+            <Route path="/review" element={<Review />}></Route>
           </Routes>
          </div>;
 }
